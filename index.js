844771e307fcd06c1ce45c8362315c23dbdb1315
Mehdi <mehdiab92@gmail.com>
2019-03-21 00:36:16 +0100

    add axios for request

--- index.js
+++ index.js
@@ -2,10 +2,49 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const axios = require('axios');
+
+var date = '20180201__20190301';
+//let id = '128838707';
+//let edition = 'fr-fr';
+var key = '6e7f71b0fa1b4f3f98c4e7299b804a61';
+var token = 'fe52d00f06904e26a1cb2c0ad9791fa3';
+
+const url = 'https://api.ozae.com/gnw/articles?date=20190317__20190317&key=6e7f71b0fa1b4f3f98c4e7299b804a61&edition=fr-fr&query=paris&hard_limit=50&order%5Bcol%5D=article_score&order%5Bsrt%5D=DESC';
+
 
 var app = express();
 
+
 app.use(cors());
+
+app.get('/requestAll/:keyword/:date', function(req, res)
+{
+    const keyword = req.params.keyword;
+    const date = req.params.date;
+
+    const edition = 'fr-fr';
+
+    const url = 'https://api.ozae.com/gnw/articles?date='+date+'&key='+key+'&edition='+edition+'&query='+keyword+'&hard_limit=0&order%5Bcol%5D=article_score&order%5Bsrt%5D=DESC';
+
+    console.log(url);
+
+    axios.get(url)
+        .then(function (response) {
+            res.send(response.data.articles);
+            // handle success
+            console.log('REPONSE SERVER');
+            console.log(response.data.articles);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+});
+
 
 app.get('/trad/:words', function(req, res) {
    const words = req.params.words;
