4482a58bc0a15da5c2428b8020946125ebacffd1
Mehdi <mehdiab92@gmail.com>
2019-03-20 12:35:16 +0100

    multiple tradm

--- index.js
+++ index.js
@@ -7,43 +7,51 @@ var app = express();
 
 app.use(cors());
 
-app.get('/trad/:fromLanguage/:toLanguage/:words', function(req, res) {
-   console.log(req.params.words);
+app.get('/trad/:words', function(req, res) {
    const words = req.params.words;
-   const fromLanguage = req.params.fromLanguage;
-   const toLanguage = req.params.toLanguage;
 
    res.send('get the correct route');
 
     async function run(keyword, fromLanguage, toLanguage) {
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: false});
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true});
         const page = await browser.newPage();
 
-        const paramsUrl = keyword.replace(' ', '%20');
+        const paramsUrl = keyword.replace(/\s+/g, '%20');
         console.log(paramsUrl);
 
         const base = 'https://www.deepl.com/translator#';
-        const url = base.concat(fromLanguage,'/',toLanguage,'/',words);
+        const url = base.concat(fromLanguage,'/',toLanguage,'/',paramsUrl);
 
         await page.goto(url);
 
-        await page.waitFor(5000);
+        await page.waitFor(1500);
 
         const resultTrad = await page.$eval('.lmt__target_textarea', el => el.value);
         console.log(resultTrad);
 
-        await page.screenshot({ path: 'screenshots/github.png' });
+        const obj = {
+            lang: toLanguage,
+            result: resultTrad,
+        };
+
+        const json = JSON.stringify(obj);
 
         fs.writeFile(
-            './json/test.json',
-            JSON.stringify(resultTrad, null, 2),
+            './json/result-'+toLanguage+'.json',
+            json,
+            'utf-8',
             (err) => err ? console.error('Data not written!', err) : console.log('Data written')
         );
 
         browser.close();
     }
 
-    run(words, fromLanguage, toLanguage);
+    run(words, 'fr', 'en');
+    run(words, 'fr', 'de');
+    run(words, 'fr', 'es');
+    run(words, 'fr', 'it');
+    run(words, 'fr', 'nl');
+    run(words, 'fr', 'pt');
 
 
 });
